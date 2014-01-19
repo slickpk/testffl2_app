79779aff31341a861f7807400498c2330ded6380
Paul Kioko <slickpk@yahoo.com>
2014-01-19 16:03:55 -0500

    Initial Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestfflApp::Application.config.secret_key_base = '312871fb01b47b10bedba7f913a836280b44c973cd43817fb91143db1ff916fcccd1d28cc84723b4e9f52f6536dcc78b8fde413261b8ec9c4d588813d5a9e7aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestfflApp::Application.config.session_store :cookie_store, key: '_testffl_app_session'
